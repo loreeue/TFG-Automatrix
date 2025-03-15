071287427a0115a419263157e29815ae98a4d721
loreeue <loretouzquianoesteban@MacBook-Pro-2.local>
2025-03-15 17:45:40 +0100

    Despliegue web

--- backend/src/main/java/org/example/Controllers/FrontendController.java
+++ backend/src/main/java/org/example/Controllers/FrontendController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendController {
-    @RequestMapping(value = { "/", "/{[path:[^\\.]*}", "/**/{[path:[^\\.]*}" })
+    @RequestMapping(value = { "/", "/{path:^(?!.*\\.).*$}", "/**/{path:^(?!.*\\.).*$}" })
     public String forward() {
         // Retorna index.html para que React Router gestione la ruta
         return "forward:/index.html";
