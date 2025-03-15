1924dec4fe772150796151b48a1e2b10c4181eff
loreeue <loretouzquianoesteban@MacBook-Pro-2.local>
2025-03-15 17:36:29 +0100

    Despliegue web

---
+++ backend/src/main/java/org/example/Controllers/FrontendController.java
@@ -0,0 +1,13 @@
+package org.example.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+    @RequestMapping(value = { "/", "/{[path:[^\\.]*}", "/**/{[path:[^\\.]*}" })
+    public String forward() {
+        // Retorna index.html para que React Router gestione la ruta
+        return "forward:/index.html";
+    }
+}
